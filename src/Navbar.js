ec9779f500d93f2685ddce8e0fc836f41045ff90
Tal Shlomo <shlomotal01@gmail.com>
2022-01-02 17:27:48 +0200

    Start connect to backend

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,50 +5,70 @@
 
 // //import "./../CSS/Navbar.css";
 // import { IconContext } from "react-icons";
-// import { Button } from "react-bootstrap";
-import React, { Component } from 'react';
-import {Link} from "react-router";
-import './Navbar.css'
+import { Button } from "react-bootstrap";
+import React, { Component } from "react";
+import { Link } from "react-router";
+import "./Navbar.css";
 
 class Navbar extends Component {
   state = { clicked: false };
 
-  handleClick=()=>{
-    this.setState({clicked:!this.state.clicked})
-  }
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  onLogout = () => {
+    localStorage.removeItem("username");
+    window.location.reload();
+  };
 
   render() {
+    const username = localStorage.getItem("username");
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Comunication_LTD</h1>
         <div className="menu-icon" onClick={this.handleClick}>
-          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
         </div>
-        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-          <li className="nav-links">
-          <a href="/home">Home</a>
-          </li>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {!username ? (
+            <>
+              <li className="nav-links-login">
+                <a href="/SignIn">Sign In</a>
+              </li>
 
-          <li className="nav-links">
-            <a href="/changepassword">Change Password</a>
-          </li>
+              <li className="nav-links-signup">
+                <a href="/SignUp">Sign Up</a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="nav-links">
+                <a href="/home">Home</a>
+              </li>
 
-          <li className="nav-links-login"> 
-            <a href="/SignIn">Sign In</a>
-          </li>
-
-          <li className="nav-links-signup">
-            <a href="/SignUp">Sign Up</a>
-          </li>
+              <li className="nav-links">
+                <a href="/changepassword">Change Password</a>
+              </li>
+              <div>
+                <p> </p>
+                <Button
+                  size="sm"
+                  variant="danger"
+                  block
+                  onClick={this.onLogout}
+                >
+                  Log Out
+                </Button>
+              </div>
+            </>
+          )}
         </ul>
       </nav>
     );
   }
 }
-
-
-// function Navbar() {
-
-// }
 
 export default Navbar;
--- src/app.js
+++ src/app.js
@@ -4,19 +4,30 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import Login from './Pages/Login/login';
 import CreateUser from "./Pages/SignUp/CreateUser";
+import Home from "./Pages/Home/home";
+
 import "./App.css";
 import { LogIn } from "react-ionicons";
 
 
 function App() {
+  const username = localStorage.getItem("username");
   return (
     <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/SignIn" element={<Login />} />
-        <Route path="/SignUp" element={<CreateUser />} />
-      </Routes>
+      {!username ? (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/SignIn" element={<Login />} />
+            <Route path="/SignUp" element={<CreateUser />} />
+          </Routes>
+        </>
+      ) : (
+          <>
+            <Navbar />
+          {/* <Route path="/home" element={<Home />} />  */}
+          </>
+      )}
     </Router>
   );
 }
